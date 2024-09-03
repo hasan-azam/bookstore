6b21b691e840268ed4e8bc80bbd118768ad47461
Hasan <hasan1117@gmail.com>
2024-09-03 00:10:14 -0700

    Implemented HTTP Get request to return single book given its ID

--- backend/index.js
+++ backend/index.js
@@ -56,6 +56,20 @@ response.status(500).send({message: error.message});
     }
 });
 
+//Route to return a specific book based on ID
+app.get('/books/:id', async (request, response) => {
+    try {
+const {id} = request.params;
+
+const book = await Book.findById(id);
+
+return response.status(200).json(book);
+    } catch (error) {
+console.log(error.message);
+response.status(500).send({message: error.message});
+    }
+});
+
 mongoose
     .connect(MongoDBURL)
     .then(() => {
